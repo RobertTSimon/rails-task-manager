a225c2f19719062c774dc0d12c7386aa018ca7b0
Robert Simon <robertsimonmedia@gmail.com>
2019-05-14 13:33:57 -0400

    displaying all tasks and added details page/links associated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # getting all tasks
+  # resources 'tasks'
+
+  get 'tasks', to: 'tasks#index'
+  # # getting one task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # # creating one task(2 requests)
+  # # 1st request is to get form
+  # # 2nd request is to post the params
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
+  # # update a task
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # # deleting a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
